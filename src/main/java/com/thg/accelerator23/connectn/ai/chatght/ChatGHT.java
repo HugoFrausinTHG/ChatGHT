e7e973ad5f280267a495fe9446fcdc3f987fe9a9
Hugo Frausin <hugo.frausin45@gmail.com>
2025-01-20 14:34:31 +0000

    added first change to make a release

--- src/main/java/com/thg/accelerator23/connectn/ai/chatght/ChatGHT.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/chatght/ChatGHT.java
@@ -1,20 +1,21 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator23.connectn.ai.chatght;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class ChatGHT extends Player {
+  public ChatGHT(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, ChatGHT.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+
+    return 5;
   }
 }
