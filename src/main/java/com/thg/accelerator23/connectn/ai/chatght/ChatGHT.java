2cc720a96d9d00243abc36dcd814f77e8ea42e24
Hugo Frausin <hugo.frausin45@gmail.com>
2025-01-24 15:14:57 +0000

    Got bot that calls Lewis' AIvinson for help

--- src/main/java/com/thg/accelerator23/connectn/ai/chatght/ChatGHT.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/chatght/ChatGHT.java
@@ -3,19 +3,20 @@ package com.thg.accelerator23.connectn.ai.chatght;
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
-
+import com.thg.accelerator20.connectn.ai.lewisivinson.AIvinson;
 
 public class ChatGHT extends Player {
+  private Counter myCounter;
   public ChatGHT(Counter counter) {
     //TODO: fill in your name here
     super(counter, ChatGHT.class.getName());
+    this.myCounter = counter;
   }
 
   @Override
   public int makeMove(Board board) {
-    //TODO: some crazy analysis
-    //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-
-    return 5;
+    System.out.println("I am going to ask Lewis' bot for help");
+    Player betterBot = new AIvinson(myCounter);
+    return betterBot.makeMove(board);
   }
 }
